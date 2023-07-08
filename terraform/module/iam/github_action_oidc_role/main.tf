ab30bc8228fef94cbbcea594acb8c79c8b8777cc
Masashi.Kayahara <masashi.kayahara@arc-connects.com>
2023-07-08 14:33:18 +0900

    Add OICD IAM role.

--- terraform/module/iam/github_action_oidc_role/main.tf
+++ terraform/module/iam/github_action_oidc_role/main.tf
@@ -51,5 +51,5 @@ data "aws_iam_policy_document" "sts" {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://${var.github_oidc_domain}"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
 }
